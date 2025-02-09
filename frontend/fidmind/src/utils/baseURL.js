7b926fe3df66ad425be4a538cd80200c690ba1ea
Bandaman <101086673+Francissha@users.noreply.github.com>
2025-02-09 05:27:08 -0500

    Update baseURL.js

--- frontend/fidmind/src/utils/baseURL.js
+++ frontend/fidmind/src/utils/baseURL.js
@@ -1,11 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const getBaseUrl = () => {
+  return 'https://xyztest.onrender.com';  // Production URL
+}
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',  // Ensure this points to the correct output folder
-    chunkSizeWarningLimit: 2000,  // Set your desired chunk size warning limit in kB
-  },
-})
+export default getBaseUrl;
