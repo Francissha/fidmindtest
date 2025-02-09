a2aa89b97c88f28327c73c3a53c457c32b4025e5
Bandaman <101086673+Francissha@users.noreply.github.com>
2025-02-09 05:32:53 -0500

    Update vite.config.js

--- frontend/fidmind/vite.config.js
+++ frontend/fidmind/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Ensure this points to the correct output folder
+    outDir: './frontend/fidmind/dist',  // Ensure this points to the correct output folder
     chunkSizeWarningLimit: 2000,  // Set your desired chunk size warning limit in kB
   },
 });
