71cbf057962ca2e634be14ef653786f04ba15aeb
Bandaman <101086673+Francissha@users.noreply.github.com>
2025-02-11 03:52:41 -0500

    Update TopSellers.jsx

--- frontend/fidmind/src/pages/home/TopSellers.jsx
+++ frontend/fidmind/src/pages/home/TopSellers.jsx
@@ -70,7 +70,7 @@ const TopSellers = () => {
       >
         {bookChunks.length > 0 ? bookChunks.map((chunk, index) => (
           <SwiperSlide key={index}>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-6">
               {chunk.map((book) => (
                 <BookCard key={book._id} book={book} />
               ))}
