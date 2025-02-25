23a10508b8ee1caec9454d3365a857e6f3e51689
Bandaman <101086673+Francissha@users.noreply.github.com>
2025-02-25 18:05:09 -0500

    Update BookCard.jsx

--- frontend/fidmind/src/pages/books/BookCard.jsx
+++ frontend/fidmind/src/pages/books/BookCard.jsx
@@ -36,11 +36,11 @@ const BookCard = ({ book }) => {
             </h3>
           </Link>
 
-          {/* Author Section  <p className="text-gray-700 text-xs sm:text-sm font-medium mb-1">
+          {/* Author Section */}
+           <p className="text-gray-700 text-xs sm:text-sm font-medium mb-1">
             Author: <span className="text-blue-600">{book.author}</span>
-          </p>*/}
+          </p>
          
-
           {/* Genre Section */}
           <p className="text-gray-700 text-xs sm:text-sm font-medium mb-1">
             Genre: {book.category}
