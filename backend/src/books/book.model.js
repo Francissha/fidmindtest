470f1b04bb3d68b280274db1e8dc606887089eac
Bandaman <101086673+Francissha@users.noreply.github.com>
2025-02-26 07:35:31 -0500

    Update book.model.js

--- backend/src/books/book.model.js
+++ backend/src/books/book.model.js
@@ -1,38 +1,44 @@
 
-const mongoose = require("mongoose");
+const { mongoose } = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-    name: { 
+const bookSchema = new mongoose.Schema({
+    title: {
         type: String,
         required: true,
     },
-    email: {
+    description: {
         type: String,
         required: true,
     },
-    address: {
-        city: { type: String, required: true },
-        country: String,
-        state: String,
-        zipcode: String,
-    },
-    phone: {
-        type: String, 
+    category: {
+        type: String,
         required: true,
     },
-    productIds: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Book",
-            required: true,  
-        }
-    ],
-    totalPrice: {
+    trending: {
+        type: Boolean,
+        default: true,
+    },
+    coverImage: {
+        type: String,
+        required: true,
+    },
+    oldPrice: {
         type: Number,
         required: true,
+    },
+    newPrice: {
+        type: Number,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
     }
-}, { timestamps: true });
+},
+{
+    timestamps: true,
+});
 
-const Order = mongoose.model("Order", orderSchema);
+const Book = mongoose.model('Book', bookSchema);
 
-module.exports = Order;
+module.exports = Book;
